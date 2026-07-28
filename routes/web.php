2018d0dbc12bcb89d94ee201ba836f4230ba056e
Cahzello <rizkyrasya.m@gmail.com>
2026-07-28 14:04:58 +0700

    feat: membuat view login dan regis. membuat controller register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::post('/logout', function () {
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+
+    return redirect('/');
+})->middleware('auth')->name('logout');
