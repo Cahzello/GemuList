3e662ba419e48597cfd159a4d893a3956500e8d5
Cahzello <rizkyrasya.m@gmail.com>
2026-07-28 15:31:35 +0700

    feat: membuat footer

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/footer', function () {
+    return view('components.footer');
+});
+
 Route::get('/test', function () {
     return view('components.navbar');
 });
