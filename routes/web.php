3f02e21aa90453d333aa8d94bfc3695aae5746a6
OriOfRivia <primaryrezafahrizki@gmail.com>
2026-08-04 23:48:58 +0700

    feat: add personal score page (posters, sort/filter, review modal)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/personalScore', function () {
+    return view('personalScore.index');
 });
