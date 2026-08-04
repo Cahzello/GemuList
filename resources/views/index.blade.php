4b9cc195a093aa4fe3101be2d8633fd644639467
Cahzello <rizkyrasya.m@gmail.com>
2026-08-04 22:53:33 +0700

    feat: navbar di my-games tapi masih rusak css nya

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html lang="id">
-
+<html lang="en">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -20,7 +19,7 @@
     </main>
 
     <x-footer />
-
+    
 </body>
 
 </html>
